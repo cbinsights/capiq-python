565c8b4dd3bf6f31117d455106f62951173002b6
Jide Fajobi <ofajobi@cbinsights.com>
2018-08-21 14:47:51 -0400

    add debug statement

--- capiq/capiq_client.py
+++ capiq/capiq_client.py
@@ -146,6 +146,11 @@ class CapIQClient:
         req = {"inputRequests": req_array}
         response = requests.post(self._endpoint, headers=self._headers, data=json.dumps(req),
                                  auth=HTTPBasicAuth(self._username, self._password), verify=self._verify)
+        print('==============================')
+        print('==============================')
+        print('response', response)
+        print('==============================')
+        print('==============================')
         if self._debug:
             logging.info("Cap IQ response")
             logging.info(response.json())
